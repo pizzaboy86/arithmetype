091d57780a6ecf4c55c0fd096d87511f44a3b06e
pizzaboy86 <lucasricciardi00@gmail.com>
2025-11-02 03:32:22 -0500

    rewrite

--- main.py
+++ main.py
@@ -1,59 +1,55 @@
 import math
 import random
 
-axioms = [
-    "^",
-]
-
-pyAxioms = [ # hashtables are too hard
-    "**"
-]
+axioms = {
+    "^" : "**",
+}
 
 templates = [
     "n*x",
     "n/x",
     "x^n",
-    "n^x"
+    "n^x",
+    "n+x",
+    "n-x",
+    "(n/x)*(n^x)"
 ]
 
-def genProblem(highestCoef, highestEval, arguments):
-    if arguments < 1:
-        arguments = 1
-    problem = ""
-    answer = 0
+def genProblem(highestCoef, highestEval):
+    coefficient = random.randint(0,highestCoef)
+    evaluation = random.randint(1,highestEval)
+
+    problem = list(templates[random.randint(0,len(templates)-1)])
+
+    for char in problem:
+        if char == "n":
+            problem[problem.index(char)] = str(coefficient)
     
-    coefficient = random.randint(0, highestCoef)
-    evaluation = random.randint(1, highestEval)
-    
-    template = list(templates[random.randint(0,len(templates)-1)]) # init problem
-    template.insert(template.index("n"), str(coefficient))
-    template.remove("n")
-    problem = "".join(template)
-    
-    for i in range(arguments-1):
-        template = list(templates[random.randint(0,len(templates)-1)])
-        template.insert(template.index("n"), str(coefficient))
-        template.remove("n")
-        problem += " + " + "".join(template)
-    
-    syntaxxedProblem = list(problem)
-    
-    counter = 0
-    for axiom in axioms:
-        if syntaxxedProblem.count(axiom) > 0:
-            syntaxxedProblem[syntaxxedProblem.index(axiom)] = pyAxioms[counter]
-        counter+=1
-    
-    for i in range(syntaxxedProblem.count("x")):
-        syntaxxedProblem.insert(syntaxxedProblem.index("x"), str(evaluation))
-        syntaxxedProblem.remove("x")
-    
-    print("Evaluated: " + str(syntaxxedProblem))
-    
-    answer = eval("".join(syntaxxedProblem))
-    problem += " when x = " + str(evaluation)
-    
-    print(problem + " : " + str(answer))
+    toEval = problem # same memory position, changes both for some reason!
+
+    for char in toEval:
+        if char == "x":
+            toEval[toEval.index(char)] = str(evaluation)
+        try: 
+            axioms[char]
+        except:
+            continue
+        else:
+            toEval[toEval.index(char)] = axioms[char]
+            continue
+
+    answer = eval("".join(toEval))
+    problem = str(problem) + " ; when x = " + str(evaluation)
+
+    print("Problem: " + "".join(problem))
+    print("Answer: " + str(answer))
     return problem, answer
+
+
+for i in range(10):
+    genProblem(10,10)
+
+    #make i/o
+
     
-genProblem(10,3,2)
+
